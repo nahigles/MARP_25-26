b27e665a749bcdb8198ab185e714d9c0d13e20b1
Nahia <82345386+nahigles@users.noreply.github.com>
2025-09-21 19:26:12 +0200

    Comienzo Ej2 Tema 1

--- Tema1/Ej2_KesimoMenorElem/Ej2_KesimoMenorElem.cpp
+++ Tema1/Ej2_KesimoMenorElem/Ej2_KesimoMenorElem.cpp
@@ -8,10 +8,10 @@
 
 #include <iostream>
 #include <fstream>
-#include <...>
+//#include <...>
 using namespace std;
 
-#include "..."  // propios o los de las estructuras de datos de clase
+#include "TreeSet_AVL.h"  // propios o los de las estructuras de datos de clase
 
 /*@ <answer>
 
@@ -22,15 +22,17 @@ using namespace std;
  @ </answer> */
 
 
-// ================================================================
-// Escribe el código completo de tu solución aquí debajo
-// ================================================================
-//@ <answer>
+ // ================================================================
+ // Escribe el código completo de tu solución aquí debajo
+ // ================================================================
+ //@ <answer>
 
 bool resuelveCaso() {
 	// leer los datos de la entrada
+	int numElems;
+	cin >> numElems;
 
-	if (caso especial)
+	if (numElems == 0)
 		return false;
 
 	// resolver el caso posiblemente llamando a otras funciones
@@ -44,7 +46,7 @@ bool resuelveCaso() {
 //  Lo que se escriba dejado de esta línea ya no forma parte de la solución.
 
 int main() {
-// ajustes para que cin extraiga directamente de un fichero
+	// ajustes para que cin extraiga directamente de un fichero
 #ifndef DOMJUDGE
 	std::ifstream in("casos.txt");
 	if (!in.is_open())
@@ -54,7 +56,7 @@ int main() {
 
 	while (resuelveCaso());
 
-// para dejar todo como estaba al principio
+	// para dejar todo como estaba al principio
 #ifndef DOMJUDGE
 	std::cin.rdbuf(cinbuf);
 	std::cout << "Pulsa Intro para salir..." << std::flush;
