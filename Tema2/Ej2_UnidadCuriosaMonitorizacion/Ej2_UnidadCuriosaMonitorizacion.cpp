86d4a3dcca4028515dad3f78d0ba1916eb92c899
Nahia <82345386+nahigles@users.noreply.github.com>
2025-09-30 13:41:45 +0200

    Ej2 con error

--- Tema2/Ej2_UnidadCuriosaMonitorizacion/Ej2_UnidadCuriosaMonitorizacion.cpp
+++ Tema2/Ej2_UnidadCuriosaMonitorizacion/Ej2_UnidadCuriosaMonitorizacion.cpp
@@ -7,10 +7,9 @@
 
 #include <iostream>
 #include <fstream>
-#include <...>
+#include <queue>
 using namespace std;
 
-#include "..."  // propios o los de las estructuras de datos de clase
 
 /*@ <answer>
 
@@ -21,20 +20,51 @@ using namespace std;
  @ </answer> */
 
 
-// ================================================================
-// Escribe el código completo de tu solución aquí debajo
-// ================================================================
-//@ <answer>
+ // ================================================================
+ // Escribe el código completo de tu solución aquí debajo
+ // ================================================================
+ //@ <answer>
 
+
+struct Registro {
+	int momento;
+	int id;
+	int periodo;
+};
+
+bool operator>(Registro const& a, Registro const& b) {
+	return b.momento < a.momento || (a.momento == b.momento && b.id < a.id);
+}
 bool resuelveCaso() {
 	// leer los datos de la entrada
+	int n;
+	cin >> n;
 
-	if (caso especial)
+	if (n == 0)
 		return false;
 
 	// resolver el caso posiblemente llamando a otras funciones
+	
+	priority_queue<Registro> cola;
+
+	for (int i = 0; i < n; i++) {
+		int idU, p;
+		cin >> idU >> p;
+		cola.push({ p,idU,p });
+	}
 
 	// escribir la solución
+	int envios;
+	cin >> envios;
+
+	while (envios--) {
+		auto e = cola.top(); cola.pop();
+		cout << e.id << '\n';
+		e.momento += e.periodo;
+		cola.push(e);
+	}
+
+	cout << "---\n";
 
 	return true;
 }
@@ -43,7 +73,7 @@ bool resuelveCaso() {
 //  Lo que se escriba dejado de esta línea ya no forma parte de la solución.
 
 int main() {
-// ajustes para que cin extraiga directamente de un fichero
+	// ajustes para que cin extraiga directamente de un fichero
 #ifndef DOMJUDGE
 	std::ifstream in("casos.txt");
 	if (!in.is_open())
@@ -53,7 +83,7 @@ int main() {
 
 	while (resuelveCaso());
 
-// para dejar todo como estaba al principio
+	// para dejar todo como estaba al principio
 #ifndef DOMJUDGE
 	std::cin.rdbuf(cinbuf);
 	std::cout << "Pulsa Intro para salir..." << std::flush;
