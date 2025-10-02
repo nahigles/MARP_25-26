2665e4f1b1fdb2e1487db946b5f252e71d9215d3
Nahia <82345386+nahigles@users.noreply.github.com>
2025-10-02 11:12:43 +0200

    Update EjL_BatallaDeAudiencias.cpp

--- Tema3/Ej3L_BatallaDeAudiencias/EjL_BatallaDeAudiencias.cpp
+++ Tema3/Ej3L_BatallaDeAudiencias/EjL_BatallaDeAudiencias.cpp
@@ -17,9 +17,15 @@ using namespace std;
 #include "IndexPQ.h"
 /*@ <answer>
 
- Sustituye este párrafo por un comentario general sobre la solución,
- explicando cómo se resuelve el problema y cuál es el coste de la solución,
- en función del tamaño del problema.
+Tenemos una cola con prioridades variables en la que guardamos el id del canal y su maxima audiencia del momento.
+Nos llegan actualizaciones de la audiencia y en un unordered_map vamos acumulando el tiempo que un canal ha tenido
+la maxima audiencia.
+Se ordena el unordered_map con el metodo sort de mayor a menor por tiempo total de audiencia maxima, y en caso de empate,
+de menor a mayor por el id del canal.
+
+Coste en tiempo:  O(C*log C) +  O(K * log K), siendo C el numero total de canales y k el numero de canales diferentes que han tenido audiencia maxima.
+
+Coste en espacio: O(C), siendo C el numero total de canales porque todos los canales estan en la cola IndexPQ y puedenestar en el unordered_map.
 
  @ </answer> */
 
@@ -40,18 +46,18 @@ bool resuelveCaso() {
 	IndexPQ<int, greater<int>> canales(C+1);
 	unordered_map<int, int> minutosPrime; // <Canal, Minutos de PrimeTime>
 
-	for (int i = 1; i <= C; i++) {
+	for (int i = 1; i <= C; i++) { // O(C*log C)
 		int audiencia;
 		cin >> audiencia;
 
-		canales.update(i, audiencia);
+		canales.update(i, audiencia); // O(log C)
 	}
 
 	for (int i = 0; i < N; i++) {
 		int min;
 		cin >> min;
 
-		auto canal_prime = canales.top();
+		auto canal_prime = canales.top(); // O(1)
 		int tiempo = min - ultimoMin;
 
 		minutosPrime[canal_prime.elem] += tiempo;
@@ -62,7 +68,7 @@ bool resuelveCaso() {
 		while (canal != -1) {
 			cin >> audiencia;
 
-			canales.update(canal, audiencia);
+			canales.update(canal, audiencia); // O(log C)
 
 			cin >> canal;
 		}
@@ -79,7 +85,7 @@ bool resuelveCaso() {
 
 	vector<pair<int, int>> v_minutosPrime(minutosPrime.begin(), minutosPrime.end());
 
-	std::sort(v_minutosPrime.begin(), v_minutosPrime.end(),
+	std::sort(v_minutosPrime.begin(), v_minutosPrime.end(), // O(K * log K) siendo K el numero de canales diferentes que han tenido audiencia maxima
 		[](const auto& a, const auto& b) {
 			if (a.second == b.second) {
 				return a.first < b.first;
@@ -87,7 +93,7 @@ bool resuelveCaso() {
 			return b.second < a.second;
 		});
 
-	for (const auto& par : v_minutosPrime) {
+	for (const auto& par : v_minutosPrime) { // O(K)
 
 		cout << par.first << " " << par.second << "\n";
 	}
