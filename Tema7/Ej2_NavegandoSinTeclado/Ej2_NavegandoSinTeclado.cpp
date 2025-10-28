ed95ce4d9189abb83c8955d14c7303d26c4f9ec5
nahigles <82345386+nahigles@users.noreply.github.com>
2025-10-28 18:01:23 +0100

    Empezando Ej 7-2

---
+++ Tema7/Ej2_NavegandoSinTeclado/Ej2_NavegandoSinTeclado.cpp
@@ -0,0 +1,115 @@
+
+/*@ <authors>
+ *
+ * MARPV28 Nahia Iglesias Calvo
+ *
+ *@ </authors> */
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+#include "DigrafoValorado.h"  // propios o los de las estructuras de datos de clase
+
+/*@ <answer>
+
+ Escribe aquí un comentario general sobre la solución, explicando cómo
+ se resuelve el problema y cuál es el coste de la solución, en función
+ del tamaño del problema.
+
+ @ </answer> */
+
+
+ // ================================================================
+ // Escribe el código completo de tu solución aquí debajo
+ // ================================================================
+ //@ <answer>
+
+template <typename Valor>
+using Camino = deque<AristaDirigida<Valor>>;
+
+template <typename Valor>
+class Dijkstra {
+public:
+	Dijkstra(DigrafoValorado<Valor> const& g, int orig) : origen(orig),
+		dist(g.V(), INF), ulti(g.V()), pq(g.V()) {
+		dist[origen] = 0;
+		pq.push(origen, 0);
+		while (!pq.empty()) {
+			int v = pq.top().elem; pq.pop();
+			for (auto a : g.ady(v))
+				relajar(a);
+		}
+	}
+
+	bool hayCamino(int v) const { return dist[v] != INF; }
+
+	Valor distancia(int v) const { return dist[v]; }
+
+	Camino<Valor> camino(int v) const {
+		Camino<Valor> cam;
+		// recuperamos el camino retrocediendo
+		AristaDirigida<Valor> a;
+		for (a = ulti[v]; a.desde() != origen; a = ulti[a.desde()])
+			cam.push_front(a);
+		cam.push_front(a);
+		return cam;
+	}
+
+private:
+	const Valor INF = std::numeric_limits<Valor>::max();
+	int origen;
+	std::vector<Valor> dist;
+	std::vector<AristaDirigida<Valor>> ulti;
+	IndexPQ<Valor> pq;
+
+	void relajar(AristaDirigida<Valor> a) {
+		int v = a.desde(), w = a.hasta();
+		if (dist[w] > dist[v] + a.valor()) {
+			dist[w] = dist[v] + a.valor();
+			ulti[w] = a;
+			pq.update(w, dist[w]);
+		}
+	}
+};
+
+
+bool resuelveCaso() {
+	// leer los datos de la entrada
+	int N; // N -> paginas
+	cin >> N;
+	if (N == 0)
+		return false;
+
+	// Leo mins de paginas
+	vector<int> mi
+	// resolver el caso posiblemente llamando a otras funciones
+
+	// escribir la solución
+
+	return true;
+}
+
+//@ </answer>
+//  Lo que se escriba dejado de esta línea ya no forma parte de la solución.
+
+int main() {
+	// ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+	std::ifstream in("casos.txt");
+	if (!in.is_open())
+		std::cout << "Error: no se ha podido abrir el archivo de entrada." << std::endl;
+	auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+
+	while (resuelveCaso());
+
+	// para dejar todo como estaba al principio
+#ifndef DOMJUDGE
+	std::cin.rdbuf(cinbuf);
+	std::cout << "Pulsa Intro para salir..." << std::flush;
+	std::cin.get();
+#endif
+	return 0;
+}
